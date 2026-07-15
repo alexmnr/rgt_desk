adcc1ef41b7b2bd0ccc64d497aa39b4b8ed9d5dc
Alexander Minor <alexander@minor.pw>
2026-07-15 05:31:14 -0400

    renamed install folder to systemd_services

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ python3 -m venv .venv
 source .venv/bin/activate
 python3 -m pip install -r requirements.txt
 
-sudo ln -sf "$REPO_DIR/install/rgt_desk_backend.service" /etc/systemd/system/rgt_desk_backend.service
+sudo ln -sf "$REPO_DIR/systemd_services/rgt_desk_backend.service" /etc/systemd/system/rgt_desk_backend.service
 sudo systemctl daemon-reload
 sudo systemctl enable rgt_desk_backend
 sudo systemctl start rgt_desk_backend
@@ -19,7 +19,7 @@ echo "Setting up Frontend..."
 cd rgt_desk_frontend
 npm install
 npm run build
-sudo ln -sf "$REPO_DIR/install/rgt_desk_frontend.service" /etc/systemd/system/rgt_desk_frontend.service
+sudo ln -sf "$REPO_DIR/systemd_services/rgt_desk_frontend.service" /etc/systemd/system/rgt_desk_frontend.service
 sudo systemctl daemon-reload
 sudo systemctl enable rgt_desk_frontend
 sudo systemctl start rgt_desk_frontend
